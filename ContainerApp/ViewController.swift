843c3d9d7a8ac7d2f2359f6b8ed1d49aa84c3cfb
cice <cice@aula1PovMacN09.local>
2017-02-22 10:43:51 +0100

    ContainerView

--- ContainerApp/ViewController.swift
+++ ContainerApp/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerViewA: UIView!
+    @IBOutlet weak var containerViewB: UIView!
+    @IBOutlet weak var mySegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.containerViewA.alpha = 1
+        self.containerViewB.alpha = 0
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +25,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func segmentChanged(_ sender: AnyObject) {
+        if mySegment.selectedSegmentIndex == 0 {
+            UIView.animate(withDuration: 1, animations: {
+                self.containerViewA.alpha = 1
+                self.containerViewB.alpha = 0
+            })
+        } else {
+            UIView.animate(withDuration: 1, animations: {
+                self.containerViewA.alpha = 0
+                self.containerViewB.alpha = 1
+        
+            })
+        }
+    }
+    
+    
 
 
 }
